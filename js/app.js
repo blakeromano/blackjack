cbea2e2ddbadb72b17b8e35a45872233d9b5aa8a
blakeromano19 <blakeromano19@gmail.com>
2021-07-09 12:56:25 -0400

    better psudeocode in app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,14 @@
-// Psuedocode
-// 1) Define our cached elements including divs, messages given to the user, betting ability etc;
-// 2) Define our required variables, this is going to include an array for our cards in the deck, players hand, dealers hand, value of bet, how much money is in the players wallet, and isWinner
-// 3) Create an initialize function which will set up the deck, this will be called after the user inputs a starting wallet value with a valid number and has submitted it where we will add that value to the wallet and hide that div.
-// 4) Create a pick a card function which will use mathrandom and math floor to pick from one of the 52 cards in a deck of cards, it will then remove that card from the deck by splicing it from the array.
-// 5) Create a start round function which will distribute the cards to the player and the dealer where the second card from the dealer will show the flip side of the card, it will also submit the value of the bet from the user and remove it from the player's wallet.
-// 6) Create a function that displays for the user if they want to hit, double down, split or stand after the cards have been dealt
-// 7) Create a function for each of the options which will execute what the player decides if the user hits a card will be added to their hand, if the user doubles down a card will be added to their hand and the value of their bet will be deducted from their bet again, stand will allow the dealer to play, split would let the user have two seperate hands and then be able to play indiviually two hands if the cards are of the same value (A king and a jack can't split but a king and a king can).
-// 8) Create a dealer play function where the dealer will hit if the value of his cards are under 16 once it is over he will stop. 
-// 9) Create a check winner function where if the value of the dealers hand is over the player's but equal to or under 21 the dealer wins, if dealers hand is over 21 but the player's isn't then the player wins, we will check if the player busts in the function laid out in 7. If the value of their cards is the same and under 21 it will be a push.
-// 10) Create a function to redistribute the winnings. If the player has won we will add to their wallet 2X the value of the bet, if they won and doubled down then they will get 4X the value of the bet. If they split and won both then they get 4X if they split win one lost one then they will get the bet back. If it is a push they will recieve the value of the bet back and if they lose they will get nothing back.
-// 11) Create a function to playAgain where it will check their wallet and if their wallet is under a certain amount they will be prompted as to if they would like to add more money to their wallet, the deck will be checked and if the amount of cards left is over 25 then the deck will not be reshuffled if it is the deck will be reshuffled, it will clear the playershand, dealershand, value of bet, and isWinner variables.
-// 12) Have a reset everything button which will reload the game so user can input a wallet balance again etc;
-// 13) Add event listeners for buttons to start game, startround, hit, split, doubledown, stand, new round buttons and invoke the needed functions in the eventlistener.  
-// CHALLENGES: If possible I would like to add the following to BlackJack: AI Users to play with, ability to split hand, Card animations for dealing and shuffling with noises, a timer for user to have to select option, Add time between card's being dealt to add some realism
 // AUDIO ELEMENTS
+// Declare Audio and set volume and loop attributes as needed
 let dealCardAudio = new Audio("./sounds/dealCard.wav")
 let casinoNoise = new Audio("./sounds/casinoNoise.mp3")
 dealCardAudio.volume = 0.2
+casinoNoise.loop = true
+casinoNoise.volume = 0.03
 
 
 // Variables
 let cardsLeft = null
-let aces = ["dA", "hA", "cA", "sA"]
 let wallet = 10000
 let bet = null
 let deck = [
@@ -57,6 +44,7 @@ let messageEl = document.querySelector("#message")
 let resetBtn = document.querySelector(".reset")
 let tableOverlayEl = document.querySelector(".table-overlay")
 // Event Listeners
+// Chip Selection event listener checks to determine if the chip they selected is a valid chip and if it is then it will add it to the bet variable and display the bet value on to the page
 chipSectionEl.addEventListener("click", function(el) {
     if (el.target.id === "10" || el.target.id === "25" || el.target.id === "50" || el.target.id === "100" || el.target.id === "500"){
     let chipSelected = Number(el.target.id)
@@ -64,13 +52,16 @@ chipSectionEl.addEventListener("click", function(el) {
     currentBetBalEl.innerHTML = `Your current Bet Balance is: $${bet}`
     }
 })
+// This button sets the value of bet back to 0 and then displays that change to the HTML
 resetBetBtnEl.addEventListener("click", () => {
     bet = 0
     currentBetBalEl.innerHTML = `Your current Bet Balance is: $${bet}`
 })
+// This submits the bet as what the player has choosen and calls the init function which starts the round
 betSubmitEl.addEventListener("click", () => {
     init()
 })
+// determines what action the player wants to take and invokes the appropriate function
 controlsEl.addEventListener("click", (el) => {
     let playerAction = el.target.id
     if (playerAction === "hit-btn") {
@@ -81,12 +72,14 @@ controlsEl.addEventListener("click", (el) => {
         playerDoubleDown()
     } else {}
 })
-
+// This resets all the variables to the initalized state, for the deck it determines if the amount of cards left is less than 75 and if it is then it adds all the cards back to the deck. Resets HTML elements back to their inital state so player can add a new bet value.
 resetBtn.addEventListener("click", () => {
+    // determines how many cards are left in the deck
     deck.forEach(deck => {
         cardsLeft = deck.length + cardsLeft
         return cardsLeft
     })
+    // if deck has left then 75 cards left then it resets the deck
     if (cardsLeft <  75) {
         deck = [
             ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"],
@@ -97,10 +90,7 @@ resetBtn.addEventListener("click", () => {
             ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"],
         ]
     }
-    tableOverlayEl.style.width = "0"
-    tableOverlayEl.style.height = "0"
-    betDivEl.style.width = "max-content"
-    betDivEl.style.height = "max-content"
+    // resets variables back to an initalized state
     playersHandValue = 0
     dealersHandValue = 0
     theyDoubledDown = false
@@ -110,6 +100,11 @@ resetBtn.addEventListener("click", () => {
     bet = 0
     aceCountDealer = 0
     aceCountPlayer = 0
+    // Initalizes CSS and HTML attributes and styles back to normal state
+    tableOverlayEl.style.width = "0"
+    tableOverlayEl.style.height = "0"
+    betDivEl.style.width = "max-content"
+    betDivEl.style.height = "max-content"
     playersHandEl.innerHTML = ""
     dealersHandEl.innerHTML = ""
     messageEl.innerHTML = "MESSAGE"
@@ -129,15 +124,18 @@ resetBtn.addEventListener("click", () => {
 
 
 // Functions
+
+//Init function is run first by submitting the bet
 function init () {
+    // starts background audio no matter if it is a valid bet
     casinoNoise.play()
-    casinoNoise.loop = true
-    casinoNoise.volume = 0.03
+    // checks to see if the bet amount is a valid bet if not then it displays a warning message
     if (bet < 25) {
         currentBetBalEl.innerHTML = `You can't Place a Bet less than $25! Add more money!`
     }else if (bet > wallet) {
         currentBetBalEl.innerHTML = `You can't bet more money then you have!`
     } else {
+        // sets the CSS/HTML for the round
         tableOverlayEl.style.width = "80vw"
         tableOverlayEl.style.height = "80vh"
         dealersHandEl.style.visibility = "inherit"
@@ -145,7 +143,6 @@ function init () {
         betDivEl.style.visibility = "hidden"
         betDivEl.style.width = "0"
         betDivEl.style.height = "0"
-        wallet = wallet - bet
         playersChoices.forEach(btn => {
             btn.style.visibility = "inherit"
             btn.style.width = "125px"
@@ -154,11 +151,14 @@ function init () {
             btn.style.margin = "5px"
             btn.style.border = "3px outset whitesmoke"
         })
+        // deducts bet from wallet and resets the cardsLeft value and calls the inital card dealing function
+        wallet = wallet - bet
         cardsLeft = null
         initalCardDealing()
     }
 }
 
+// This function picks a random card from the deck, removes that card from the deck and stores it in a variable
 function pickCard() {
     let deckPickedIdx = Math.floor(Math.random() * deck.length)
     let cardPickedIdx = Math.floor(Math.random() * deck[deckPickedIdx].length)
@@ -166,13 +166,16 @@ function pickCard() {
     deck[deckPickedIdx].splice(cardPickedIdx, 1)
 }
 
+// called after the init function is done
 function initalCardDealing() {
+    // plays audio for card dealing, picks a card, determines the value for the specific player, makes a new div for the card to be held in for the specific player then adds the card as a class which renders it on to the screen
     dealCardAudio.play()
     pickCard()
     determineCardValue(card, "dealer")
     dealersHandValue = dealersHandValue + cardValue
     makeNewCardDiv("dealer")
     newDiv.classList.add(card)
+    // does the same thing over again but within a timeout to add realism and make sounds play synchonously, does this 3 times because each player starts with two cards
     setTimeout(() => {
         dealCardAudio.play()
         pickCard()
@@ -202,6 +205,7 @@ function initalCardDealing() {
         makeNewCardDiv("player")
         newDiv.classList.add(card)
     }, 3000)
+    // Checks to see if a player got black jack or if the dealer got two aces it deals with it. If both player and dealer get black jack they push and it renders it automatically
     setTimeout(() => {
         if (playersHandValue === 21) {
             renderWinner("playerWin")
@@ -216,6 +220,7 @@ function initalCardDealing() {
     }, 3001)
 }
 
+// creates a new div and stores in a variable, sees who is getting the div and adds the div to their parent node and gives them classes to make it a card and to make it a large card
 function makeNewCardDiv (personRecievingCard) {
     newDiv = document.createElement("div")
     if (personRecievingCard === "player") {
@@ -227,8 +232,10 @@ function makeNewCardDiv (personRecievingCard) {
     }
 }
 
+// Determines value of the card being dealt to the specific player
 function determineCardValue (card, p) {
     if (card === "dA" || card === "hA" || card ==="cA" || card === "sA"){
+        // determines palyer getting assigned an ace then it sets the value to 11 automatically and it increases the aceCount by 1 for the given player
         if (p === "player") {
             aceCountPlayer ++
             cardValue = 11
@@ -272,6 +279,7 @@ function determineCardValue (card, p) {
 }
 // has player take a new card 
 function playerHit () {
+    // Hides the doubledown button, plays a deal card, renders it, adds cardValue to handValue
     document.querySelector("#double-down-btn").style.visibility = "hidden"
     dealCardAudio.play()
     pickCard()
@@ -279,6 +287,7 @@ function playerHit () {
     newDiv.classList.add(card)
     determineCardValue(card, "player")
     playersHandValue = playersHandValue + cardValue
+    // checks to see if handValue is greater then 21, before it busts it checks to see if the player has an ace, if they do it decreases the value of their hand by 10 (there for setting the value of the ace to 1) then decreasing the acecount by 1
     if (playersHandValue > 21) {
         if(aceCountPlayer > 0) {
             playersHandValue = playersHandValue - 10
@@ -292,7 +301,7 @@ function playerHit () {
 function playerStand () {
     dealerPlay()
 }
-// doubles player bet and automatically makes them stand after hitting
+// doubles player bet and automatically makes them stand after hitting, does same thing as hit otherwise.
 function playerDoubleDown() {
     dealCardAudio.play()
     wallet = wallet - bet
@@ -302,6 +311,7 @@ function playerDoubleDown() {
     newDiv.classList.add(card)
     determineCardValue(card)
     playersHandValue = playersHandValue + cardValue
+    // checks to see if handValue is greater then 21, before it busts it checks to see if the player has an ace, if they do it decreases the value of their hand by 10 (there for setting the value of the ace to 1) then decreasing the acecount by 1
     if (playersHandValue > 21) {
         if(aceCountPlayer > 0) {
             playersHandValue = playersHandValue - 10
@@ -313,9 +323,9 @@ function playerDoubleDown() {
         dealerPlay()
     }
 }
-// Shows that player won
-
+// renders the win state
 function renderWinner(winStatus) {
+    // sets HTML/CSS to render that there is a winner
     resetBtn.style.visibility = "inherit"
     resetBtn.style.width = "125px"
     resetBtn.style.height = "75px"
@@ -331,6 +341,7 @@ function renderWinner(winStatus) {
         btn.style.margin = "0"
         btn.style.border = "0px outset whitesmoke"
     })
+    // sets win message depending on what winStatus was passed into the function and also puts winnings into wallet if there is any money earned
     if (winStatus === "playerLose") {
         messageEl.innerHTML = "DEALER WON!"
     } else if (winStatus === "playerWin" && theyDoubledDown === true) {
@@ -347,6 +358,7 @@ function renderWinner(winStatus) {
 
 // Dealer has to play once player stands or if they double down, only done if player hasn't busted
 function dealerPlay() {
+    //hides players choices buttons
     playersChoices.forEach(btn => {
         btn.style.visibility = "hidden"
         btn.style.width = "0"
@@ -355,6 +367,7 @@ function dealerPlay() {
         btn.style.margin = "0"
         btn.style.border = "0px outset whitesmoke"
     })
+    // flips mystery card over
     let mysteryCardEl = document.querySelector(".mystery-card")
     mysteryCardEl.remove()
     makeNewCardDiv("dealer")
@@ -367,6 +380,7 @@ function dealerPlay() {
         dealersHandValue = dealersHandValue + cardValue
         makeNewCardDiv("dealer")
         newDiv.classList.add(card)
+        // checks to see if handValue is greater then 21, before it busts it checks to see if the player has an ace, if they do it decreases the value of their hand by 10 (there for setting the value of the ace to 1) then decreasing the acecount by 1
         if (dealersHandValue > 21) {
             if (aceCountDealer > 0) {
                 dealersHandValue = dealersHandValue - 10
@@ -376,12 +390,13 @@ function dealerPlay() {
                 renderWinner("playerWin")
             }
         }
+    // if dealer hasn't bust and no winner has been determined then it determines the winner by who has highest hand value
     }
     if (messageEl.innerHTML === "MESSAGE") {
         determineWinner()
     }
 }
-// Determines winner if neither dealer or player bust
+// Determines winner by hand value, if dealer is higher dealer wins, if player is higher player wins, and if they are equal it is a push
 function determineWinner() {
     if (dealersHandValue > playersHandValue) {
         renderWinner("playerLose")
